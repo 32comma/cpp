aa032d3a8e75e1c401fa079d7e14c5713851c53f
mikuraji <102578455+mikuraji@users.noreply.github.com>
2022-04-02 12:31:00 +0900

    Create comment.cpp

---
+++ comment.cpp
@@ -0,0 +1,10 @@
+// https://www.w3schools.com/cpp/cpp_comments.asp
+#include <iostream>
+using namespace std;
+int main(int argc, char** argv) {
+	cout << "Hello world!" << endl; 
+	// This is a comment
+	/* The code below will print the words Hello World!
+	to the screen, and it is amazing */
+	cout << "I am learning C++";
+}
